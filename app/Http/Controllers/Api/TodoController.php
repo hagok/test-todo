b223acb867635da55f4711066f54c9b006961a3b
hagok <hagok@bk.ru>
2025-04-16 13:15:10 +0400

    api-done

---
+++ app/Http/Controllers/Api/TodoController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreTodoRequest;
+use App\Http\Requests\UpdateTodoRequest;
+use App\Http\Resources\Api\TodoResource;
+use App\Models\Todo;
+
+class TodoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return TodoResource::collection(Todo::all());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreTodoRequest $request)
+    {
+        return new TodoResource(Todo::create($request->all()));
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Todo $todo)
+    {
+        return new TodoResource($todo);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateTodoRequest $request, Todo $todo)
+    {
+        $todo->update($request->all());
+        return new TodoResource($todo);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Todo $todo)
+    {
+        $todo->delete();
+        return response()->json([
+            'message' => 'Item removed successfully'
+        ]);
+    }
+}
